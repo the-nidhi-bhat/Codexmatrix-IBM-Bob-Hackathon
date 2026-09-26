92f63c8d2e645e8bd7479bd21c7a5efa345c523b
Nidhi Bhat <nidhibhat@ibm-bob-hackathon.com>
2026-09-26 16:45:25 +0530

    Revert "refactor(game): replace node-uuid with uuid 9.0.1 (F-11)"
    This reverts commit 3889bec2b6b445c267efab943d4fe89f6d08e7a2.

--- server/game/index.js
+++ server/game/index.js
@@ -10,7 +10,7 @@
  */
 
 var parser = require('node-expression-eval');
-var uuidv4 = require('uuid').v4;
+var uuid = require('node-uuid');
 var Timer = require('./timer');
 var cards = require('./cards');
 var config = require('./config');
@@ -18,7 +18,7 @@ var config = require('./config');
 var Game = function (io) {	
 	/** Set initial values */
 	var playerCount = 0;
-	var gameId = uuidv4();
+	var gameId = uuid.v4();
 	var gameCard = getRandomCard();
 	
 	/** Configure the game's internal timer */
